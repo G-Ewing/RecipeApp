beb1f9bd4101abc088b8448e0c38c7598b59681a
Guy <guy.r.ewing@gmail.com>
2021-07-14 12:53:41 -0400

    Additional Search Results

--- home.js
+++ home.js
@@ -13,7 +13,7 @@ searchForm.addEventListener("submit", (e) => {
   });
   
   async function fetchAPI (){
-    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_KEY}&to=21`;
     const response = await fetch(baseURL);
     const data = await response.json();
     //generateHTML(data.hits) is going to pass the data to the funcition
